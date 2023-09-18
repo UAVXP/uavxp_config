00836c9e20594595c5871842dae224776afae555
UAVXP <uavxp29@gmail.com>
2023-09-18 22:17:58 +0300

    Improve batch config menu

--- config.bat
+++ config.bat
@@ -15,34 +15,61 @@ echo. 2>!filename!
 
 
 
+set cpu=
+set gpu=
+set minfps=
+set network=
+
+
+
 :menu1
 cls
 set /p buff=Is your CPU good (Y/N): 
-if "!buff!"=="Y" call :append uavxp_highend_cpu && goto menu2
-if "!buff!"=="y" call :append uavxp_highend_cpu && goto menu2
-if "!buff!"=="N" call :append uavxp_lowend_cpu && goto menu2
-if "!buff!"=="n" call :append uavxp_lowend_cpu && goto menu2
+if "!buff!"=="Y" set cpu=highend&& goto menu2
+if "!buff!"=="y" set cpu=highend&& goto menu2
+if "!buff!"=="N" set cpu=lowend&& goto menu2
+if "!buff!"=="n" set cpu=lowend&& goto menu2
 goto menu1
 
 :menu2
 cls
 set /p buff=Is your GPU good (Y/N): 
-if "!buff!"=="Y" call :append uavxp_highend_gpu && goto menu3
-if "!buff!"=="y" call :append uavxp_highend_gpu && goto menu3
-if "!buff!"=="N" call :append uavxp_lowend_gpu && goto menu3
-if "!buff!"=="n" call :append uavxp_lowend_gpu && goto menu3
+if "!buff!"=="Y" set gpu=highend&& goto menu3
+if "!buff!"=="y" set gpu=highend&& goto menu3
+if "!buff!"=="N" set gpu=lowend&& goto menu3
+if "!buff!"=="n" set gpu=lowend&& goto menu3
 goto menu2
 
 :menu3
 cls
-set /p buff=What is your lowest FPS in-game: 
-call :append cl_cmdrate !buff!
-call :append cl_updaterate !buff!
-call :append cl_interp 0
+set /p minfps=What is your average FPS in-game (10-60): 
+if !minfps! lss 10 echo Value is too low. Please, try again.&& pause && goto menu3
+if !minfps! gtr 60 echo Value is too high. Please, try again.&& pause && goto menu3
+REM FIXME: Value can be char or string
 goto menu4
 
-
 :menu4
+cls
+set /p buff=Is your network good (e.q. wired connection) (Y/N): 
+if "!buff!"=="Y" set network=highend&& goto menu5
+if "!buff!"=="y" set network=highend&& goto menu5
+if "!buff!"=="N" set network=lowend&& goto menu5
+if "!buff!"=="n" set network=lowend&& goto menu5
+goto menu4
+
+:menu5
+if "!cpu!"=="highend" call :append uavxp_highend_cpu
+if "!cpu!"=="lowend" call :append uavxp_lowend_cpu
+if "!gpu!"=="highend" call :append uavxp_highend_gpu
+if "!gpu!"=="lowend" call :append uavxp_lowend_gpu
+call :append cl_cmdrate !minfps!
+call :append cl_updaterate !minfps!
+if "!network!"=="highend" call :append cl_interp_ratio 1
+if "!network!"=="lowend" call :append cl_interp_ratio 2
+call :append cl_interp 0
+goto end
+
+:end
 cls
 echo Check your custom config here:
 echo.
